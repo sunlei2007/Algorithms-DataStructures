dfad63c04481ed4a1260c27c6e4c4147113c7859
Lei Sun <139188566@qq.com>
2023-01-10 10:27:44 -0600

    Modify

--- Algorithms-DataStructures/Program.cs
+++ Algorithms-DataStructures/Program.cs
@@ -1,22 +1,39 @@
 ﻿Console.WriteLine("Please enter a number:");
 int arrLength = Int32.Parse(Console.ReadLine());
+
+while(arrLength<=0)
+{
+    Console.WriteLine("Sorry,please enter a positive value.");
+    arrLength = Int32.Parse(Console.ReadLine());
+}
 string[] arr = new string[arrLength];
 
-Console.WriteLine("Please enter 3 words:");
 for (int i = 0; i<arr.Length ;i++)
 {
-    string inputWord = Console.ReadLine();
-    if (inputWord.Contains(" "))
+    string inputWord = "";
+    while(inputWord.Length<1)
     {
-        break;
+        Console.WriteLine($"Please enter word {i+1}");
+        inputWord = Console.ReadLine();
+       
     }
-    arr[i] = inputWord;
+    while (inputWord.Contains(" "))
+    {
+        Console.WriteLine($"The word cannot contain whitespace!");
+        inputWord = Console.ReadLine();
+    }
+     arr[i] = inputWord;
 }
 
 
 Console.WriteLine("Please enter a character:");
-char searchChar;
-enterSearchChar();
+char searchChar = Console.ReadKey().KeyChar;
+while (!char.IsLetter(searchChar))
+{
+    Console.WriteLine();
+    Console.WriteLine("Cannot enter number or symbol , please enter again:");
+    searchChar = Console.ReadKey().KeyChar;
+}
 
 Console.WriteLine();
 char[] newStr = string.Join("", arr).ToCharArray();
@@ -32,20 +49,3 @@ for(int i=0;i<newStr.Length;i++)
 double ratio = Math.Round((double)count/(double)newStr.Length, 2) * 100;
 
 Console.WriteLine($"The letter ‘{searchChar}’ appears {count} times in the array. This letter makes up more than {ratio}% of the total number of characters.");
-
- void enterSearchChar()
-{
-    
-    char newSearchChar= Console.ReadKey().KeyChar;
-    if (!char.IsLetter(newSearchChar))
-    {
-        Console.WriteLine();
-        Console.WriteLine("Cannot enter number or symbol , Please enter again:");
-        enterSearchChar();
-    }
-    else
-    {
-        searchChar = newSearchChar;
-    }
-   
-}
