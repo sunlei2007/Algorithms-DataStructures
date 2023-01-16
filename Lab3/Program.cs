d9fc6e7de7db70ad8bbc9900d822ec20aab23c4b
Lei Sun <139188566@qq.com>
2023-01-16 09:03:39 -0600

    Create Lab3 branch

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Collections;
+
+int[] intArr = new int[] { 1, 2, 3, 4, 2, 4 };
+ArrayList arrResult = new ArrayList();
+foreach (var item in intArr)
+{
+    List<int> tempArr = intArr.ToList<int>();
+    tempArr.Remove(item);
+
+    foreach (var itemNew in tempArr)
+    {
+        if (itemNew == item)
+        {
+            arrResult.Add(item);
+        }
+    }
+}
+Console.WriteLine(arrResult);
